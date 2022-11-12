8514e8a9bcb9f5f23c774eda34a202370ca80ca2
nikunj2706 <nikunjsth5@gmail.com>
2022-11-12 22:58:12 +0000

    timer functionality, main game scene linked

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HealthBar : MonoBehaviour
 {
@@ -16,7 +17,12 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //elapsedTime += Time.deltaTime;
-        //healthBar.fillAmount = (healthBar.fillAmount - Time.deltaTime);
+        elapsedTime += Time.deltaTime;
+        healthBar.fillAmount = (healthBar.fillAmount - Time.deltaTime*0.01f);
+        if (healthBar.fillAmount==0)
+        {
+            Debug.Log("Game Over");
+            SceneManager.LoadScene(0);
+        }
     }
 }
